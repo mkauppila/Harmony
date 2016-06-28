0e93eeaf7ca9ad91537647698a0995c92b47b4b9
Markus Kauppila <markus.kauppila@gmail.com>
2016-06-29 00:14:01 +0300

    Delegate key events from the view to the view controller

--- Harmony/GameViewController.swift
+++ Harmony/GameViewController.swift
@@ -10,7 +10,14 @@ import Cocoa
 import MetalKit
 import GLKit
 
+protocol KeyboardInputDelegate {
+    func keyUp(event: NSEvent)
+    func keyDown(event: NSEvent)
+}
+
 class InputView: MTKView {
+    var inputDelegate: KeyboardInputDelegate?
+
     required init(coder: NSCoder) {
         super.init(coder: coder)
         
@@ -21,10 +28,10 @@ class InputView: MTKView {
     }
     
     override func keyUp(theEvent: NSEvent) {
-        Swift.print("hello key up")
+        inputDelegate?.keyUp(theEvent)
     }
     override func keyDown(theEvent: NSEvent) {
-        Swift.print("hello key down")
+        inputDelegate?.keyDown(theEvent)
     }
 
     override var acceptsFirstResponder: Bool {
@@ -35,9 +42,22 @@ class InputView: MTKView {
 func GLKMatrix4ToUnsafePointer(matrix: GLKMatrix4) -> UnsafePointer<Float> {
     let a = Array(arrayLiteral: matrix.m)
     return UnsafePointer<Float>(a)
+enum KeyCode: UInt16 {
+    case A = 0
+    case D = 2
+    case W = 13
+    case S = 1
+    case Q = 12
+    case E = 14
+
+    case UpArrow = 126
+    case DownArrow = 125
+    case LeftArrow = 123
+    case RightArrow = 124
+}
 }
 
-class GameViewController: NSViewController, MTKViewDelegate {
+class GameViewController: NSViewController, MTKViewDelegate, KeyboardInputDelegate {
     
     var device: MTLDevice!
     
@@ -45,7 +65,9 @@ class GameViewController: NSViewController, MTKViewDelegate {
     var defaultPipelineState: MTLRenderPipelineState!
     var blackAndWhitePipelineState: MTLRenderPipelineState!
     var vertexBuffer: MTLBuffer!
+
     var projectionMatrix: GLKMatrix4!
+    var worldMatrix: GLKMatrix4!
 
     var redTriangle: Triangle!
     var greenTriangle: Triangle!
@@ -61,13 +83,47 @@ class GameViewController: NSViewController, MTKViewDelegate {
         }
 
         // setup view properties
-        let view = self.view as! MTKView
+        let view = self.view as! InputView
         view.delegate = self
         view.device = device
         view.sampleCount = 4
         view.becomeFirstResponder()
-        
+
+        view.inputDelegate = self
         loadAssets()
+    }
+
+    override func keyUp(event: NSEvent) {
+    }
+
+    override func keyDown(event: NSEvent) {
+        guard let keyCode = KeyCode(rawValue: event.keyCode) else {
+            print("Not supported keyCode \(event.keyCode)")
+            return;
+        }
+
+        switch keyCode {
+        case .A:
+            fallthrough
+        case .LeftArrow:
+            redTriangle.position = GLKVector3Add(redTriangle.position, GLKVector3Make(0.1, 0.0, 0.0))
+        case .D:
+            fallthrough
+        case .RightArrow:
+            redTriangle.position = GLKVector3Add(redTriangle.position, GLKVector3Make(-0.1, 0.0, 0.0))
+        case .W:
+            fallthrough
+        case .UpArrow:
+            redTriangle.position = GLKVector3Add(redTriangle.position, GLKVector3Make(0.0, 0.0, -0.01))
+        case .S:
+            fallthrough
+        case .DownArrow:
+            redTriangle.position = GLKVector3Add(redTriangle.position, GLKVector3Make(0.0, 0.0, 0.01))
+        default:
+            print("nothing")
+        }
+
+        print("model matrix \(NSStringFromGLKVector3(redTriangle.position))")
     }
     
     func loadAssets() {
@@ -78,15 +134,25 @@ class GameViewController: NSViewController, MTKViewDelegate {
         defaultPipelineState = createRenderingPipelineWithVertexShader("basic_vertex", fragmentShaderName: "basic_fragment", library: defaultLibrary)
         blackAndWhitePipelineState = createRenderingPipelineWithVertexShader("basic_vertex", fragmentShaderName: "bw_fragment", library: defaultLibrary)
 
+        projectionMatrix = createProjectionMatrix()
+        worldMatrix = createWorldMatrix()
 
+        commandQueue = device.newCommandQueue()
+        commandQueue.label = "main command queue"
+    }
+
+    func createProjectionMatrix() -> GLKMatrix4 {
         let fovyRadians: Float = Float(M_PI * 0.66)
         let aspectRatio: Float = Float(CGRectGetWidth(self.view.frame) / CGRectGetHeight(self.view.frame))
         let nearZ: Float = 0.1
         let farZ: Float = 100.0
-        projectionMatrix = GLKMatrix4MakePerspective(fovyRadians, aspectRatio, nearZ, farZ)
+        return GLKMatrix4MakePerspective(fovyRadians, aspectRatio, nearZ, farZ)
+    }
 
-        commandQueue = device.newCommandQueue()
-        commandQueue.label = "main command queue"
+    func createWorldMatrix() -> GLKMatrix4 {
+        var matrix = GLKMatrix4New()
+        matrix = GLKMatrix4RotateY(matrix, Float(-M_PI_2/10))
+        return matrix;
     }
 
     func createRenderingPipelineWithVertexShader(vertexShaderName: String, fragmentShaderName: String,  library: MTLLibrary) -> MTLRenderPipelineState? {
@@ -136,15 +202,16 @@ class GameViewController: NSViewController, MTKViewDelegate {
     }
 
     func createUniformMatrixFor(triangle: Triangle) -> MTLBuffer {
-        let matrix = triangle.modelMatrix()
-        let sizeOfMatrix4x4 = 16
+        let modelMatrix = triangle.modelMatrix()
+        let transformedModelMatrix = GLKMatrix4Multiply(worldMatrix, modelMatrix)
 
+        let sizeOfMatrix4x4 = 16
         let sizeOfSingleMatrix = sizeof(Float) * sizeOfMatrix4x4
         let sizeOfUniformBuffer = sizeOfSingleMatrix * 2
 
         let uniformBuffer = device.newBufferWithLength(sizeOfUniformBuffer, options: MTLResourceOptions.CPUCacheModeDefaultCache)
         let uniformContents = uniformBuffer.contents()
-        memcpy(uniformContents, GLKMatrix4ToUnsafePointer(matrix), sizeOfSingleMatrix)
+        memcpy(uniformContents, GLKMatrix4ToUnsafePointer(transformedModelMatrix), sizeOfSingleMatrix)
         memcpy(uniformContents + sizeOfSingleMatrix, GLKMatrix4ToUnsafePointer(self.projectionMatrix), sizeOfSingleMatrix)
         return uniformBuffer
     }
---
+++ Harmony/Utilities.swift
@@ -0,0 +1,9 @@
+//
+//  Utilities.swift
+//  Harmony
+//
+//  Created by Markus Kauppila on 28/06/16.
+//  Copyright © 2016 Markus Kauppila. All rights reserved.
+//
+
+import Foundation
